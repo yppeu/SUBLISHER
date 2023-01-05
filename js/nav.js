d44a6c319b9ebb9ec9335bcb9ccb821fb8b5a0ad
sua yeon <yppeu@naver.com>
2023-01-05 19:01:18 +0900

    0105

---
+++ js/nav.js
@@ -0,0 +1,25 @@
+// document.ready를 쓰면
+// 브라우저가 문서를 읽을 때 바디안에 있는 내용을 먼저 읽고 실행한다.
+// 이게 없으면 바디안에 내용을 안 읽고 js먼저 실행된다. (순차적으로.)
+// document.ready를 안 쓰려면 js를 바디 맨 밑에 넣어두면 된다. (순차적으로.)
+$(function () {
+  $(".navbar_block").click(function () {
+    console.log(1);
+    $(".navbar_menu").toggleClass("active");
+    $(".navbar_none").toggleClass("active");
+    $(".navbar_block").toggleClass("active");
+  });
+  $(".navbar_none").click(function () {
+    console.log(1);
+    $(".navbar_menu").toggleClass("active");
+    $(".navbar_none").toggleClass("active");
+    $(".navbar_block").toggleClass("active");
+  });
+
+  $(".navbar_menu").click(function () {
+    alert(1);
+    $(".navbar_menu").hide();
+    $(".navbar_none").hide();
+    // $(".navbar_menu").show();
+  });
+});
