f5c85903ce9dc65b79d35a456e1ea580131677ac
sua yeon <yppeu@naver.com>
2023-01-07 18:48:05 +0900

    0107

--- js/nav.js
+++ js/nav.js
@@ -3,6 +3,9 @@
 // 이게 없으면 바디안에 내용을 안 읽고 js먼저 실행된다. (순차적으로.)
 // document.ready를 안 쓰려면 js를 바디 맨 밑에 넣어두면 된다. (순차적으로.)
 $(function () {
+  window.addEventListener("scroll", function () {
+    console.log(window.scrollY);
+  });
   // 열기 아이콘
   $(".navbar_icon").click(function () {
     console.log(1);
@@ -17,13 +20,13 @@ $(function () {
   //   $(".navbar_block").toggleClass("active");
   // });
 
-  // 메뉴 리스트
-  $(".navbar_menu").click(function () {
-    console.log(1);
-    $(".navbar_block").show();
-    $(".navbar_block").toggleClass("active");
-    $(".navbar_menu").hide();
-    $(".navbar_none").hide();
-    // $(".navbar_menu").show();
-  });
+  // // 메뉴 리스트
+  // $(".navbar_menu").click(function () {
+  //   console.log(1);
+  //   $(".navbar_block").show();
+  //   $(".navbar_block").toggleClass("active");
+  //   $(".navbar_menu").hide();
+  //   $(".navbar_none").hide();
+  //   // $(".navbar_menu").show();
+  // });
 });
