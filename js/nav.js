b1fd24fdb195c0bb94dc83ae7769dbe881cf5d30
sua yeon <yppeu@naver.com>
2023-01-08 00:30:45 +0900

    0108

--- js/nav.js
+++ js/nav.js
@@ -4,11 +4,11 @@
 // document.ready를 안 쓰려면 js를 바디 맨 밑에 넣어두면 된다. (순차적으로.)
 $(function () {
   // 열기 아이콘
-  $(".navbar_icon").click(function () {
-    console.log(1);
-    $(".navbar_menu").toggleClass("active");
-    $(".navbar_icon").toggleClass("active");
-  });
+  // $(".navbar_icon").click(function () {
+  //   console.log(1);
+  //   $(".navbar_menu").toggleClass("active");
+  //   $(".navbar_icon").toggleClass("active");
+  // });
   // 닫기 아이콘
   // $(".navbar_none").click(function () {
   //   console.log(1);
@@ -16,7 +16,6 @@ $(function () {
   //   $(".navbar_none").toggleClass("active");
   //   $(".navbar_block").toggleClass("active");
   // });
-
   // // 메뉴 리스트
   // $(".navbar_menu").click(function () {
   //   console.log(1);
--- js/scroll.js
+++ js/scroll.js
@@ -1,26 +1,54 @@
 $(function () {
   console.log(window.scrollY);
 
-  $(".scroll01").click(function () {
-    console.log(1);
-    window.scrollBy(0, 825);
-    //$("window").scrollTop(836);
-  });
+  //   $(".scroll01").click(function () {
+  //     console.log(1);
+  //     console.log(window.scrollY);
+  //     window.scrollBy(0, 825);
+  //     $("window").scrollTop(836);
+  //   });
   $(window).scroll(function () {
+    console.log(window.scrollY);
     $("body").on("mousewheel", function (e) {
       $(".scroll").text(Math.ceil(window.scrollY));
-      let wheel = e.originalEvent.wheelDelta;
-      if (wheel > 0) {
-        //스크롤 올릴때
-        $(".top").fadeIn();
-        console.log("올리는 중");
-        console.log(window.scrollY);
-      } else {
-        //스크롤 내릴때
-        $(".top").fadeOut();
-        console.log("내리는 중");
-        console.log(window.scrollY);
+      let wheel = Math.ceil(window.scrollY);
+      console.log(wheel);
+      if (wheel >= 700) {
+        $(".navbar_icon").css("color", "#FFD639");
+      } else if (wheel >= 1500) {
+        $(".navbar_icon").css("color", "#00283D");
+      } else if (wheel >= 2400) {
+        $(".navbar_icon").css("color", "#FFD639");
+      } else if (wheel >= 3000) {
+        $(".navbar_icon").css("color", "#00283D");
+      } else if (wheel == 3700) {
+        // $(".navbar_icon").attr("src", "./img/play.png");
+        $(".navbar_block").click(function () {
+          alert(1);
+          $(".navbar_block").hide();
+          $(".navbar_none").show();
+          $("html, body").animate(
+            {
+              scrollTop: 0,
+            },
+            400
+          );
+          return false;
+        });
       }
+      //   $(".scroll").text(Math.ceil(window.scrollY));
+      //   //originalEvent : jQuery 가 감싸지 않은 기존 이벤트를 접근
+      //   let wheel = e.originalEvent.wheelDelta;
+      //   if (wheel > 0) {
+      //     //스크롤 올릴때
+      //     console.log("올리는 중");
+      //   } else if (wheel == 700) {
+      //     alert(1);
+      //     $(".navbar_block").css("color", "red");
+      //   } else {
+      //     //스크롤 내릴때
+      //     console.log("내리는 중");
+      //   }
     });
   });
 });
