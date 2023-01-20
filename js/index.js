68f0d489b67be90c191f31527e4b1c214b263f07
sua yeon <yppeu@naver.com>
2023-01-20 13:50:17 +0900

    0120

--- js/index.js
+++ js/index.js
@@ -1,6 +1,13 @@
 $(function () {
+  //데일리로그
   $("#btn_pj").click(function () {
     window.open("https://daily-log.co.kr/");
+    // $(location).attr("href", "https://daily-log.co.kr/");
+  });
+  //델리뮤직
+  $("#btn_pj2").click(function () {
+    console.log(1);
+    window.open("https://yppeu.github.io/DEIL/");
     // $(location).attr("href", "https://daily-log.co.kr/");
   });
 });
